6c50aa60440e24e079599f77e86d435dc8162be7
WilJoey Lin <wiljoey@gmail.com>
2015-04-20 13:55:53 +0800

    add modified time to datasetlist api

--- ckanext/tnext/controllers/Datasets.py
+++ ckanext/tnext/controllers/Datasets.py
@@ -19,11 +19,12 @@ SELECT p.id, p.name, p.title, p.url, p.notes, p.maintainer, p.maintainer_email, 
   (select value from package_extra where package_id=p.id and key='資料集類型' and state='active') as extra0,
   (select value from package_extra where package_id=p.id and key='資料量' and state='active') as extra1,
   (select value from package_extra where package_id=p.id and key='計費方式' and state='active') as extra3,
-  g.id as org_id, g.name as org_name, g.title as org_title
+  g.id as org_id, g.name as org_name, g.title as org_title, 
+  to_char(p.metadata_modified, 'YYYY-MM-DD HH24:MI:SS') as modified
 FROM public."package" p LEFT OUTER JOIN public."license" l on l.id=p.license_id, public."group" g
 WHERE p.owner_org = g.id AND p.state='active' AND p.private = FALSE AND g.state='active' 
-ORDER BY org_title, title ; '''
-        _ViewCount = collections.namedtuple("ViewCount", "id name title url notes maintainer maintainer_email license extra0 extra1 extra3 org_id org_name org_title")
+ORDER BY p.metadata_modified DESC ; '''
+        _ViewCount = collections.namedtuple("ViewCount", "id name title url notes maintainer maintainer_email license extra0 extra1 extra3 org_id org_name org_title modified")
         engine = model.meta.engine
         result = [_ViewCount(*t) for t in engine.execute(sql).fetchall()]
         model.meta.engine.execute(sql)
